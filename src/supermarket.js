894a52b6cacfcd642aca691b21280198619bf239
marcelopereirascmspain <marcelo.pereira@AFE0804061.local>
2015-03-26 21:56:53 +0100

    first tests

--- src/supermarket.js
+++ src/supermarket.js
@@ -1,6 +1,14 @@
 
 module.exports = {
-  getPriceFor: function () {
-    return 84;
+  getPriceFor: function (product) {
+    switch (product) {
+      case "A": return 84;
+      case "B": return 13;
+      case "C": return 62;
+      case "D": return 64;
+      case "E": return 2;
+      case "F": return 59;
+      default: throw new Error("Non Existent Product");
+    }
   }
 }
--- test/supermarket-test.js
+++ test/supermarket-test.js
@@ -5,4 +5,34 @@ describe("supermarket pricing", function function_name (argument) {
   it("it should return the price 84 for the product A", function () {
     expect(Supermarket.getPriceFor("A")).toEqual(84);
   });
+
+  it("it should return the price 13 for the product B", function () {
+    expect(Supermarket.getPriceFor("B")).toEqual(13);
+  });
+
+  it("it should return the price 62 for the product C", function () {
+    expect(Supermarket.getPriceFor("C")).toEqual(62);
+  });
+
+  it("it should return the price 64 for the product D", function () {
+    expect(Supermarket.getPriceFor("D")).toEqual(64);
+  });
+
+  it("it should return the price 2 for the product E", function () {
+    expect(Supermarket.getPriceFor("E")).toEqual(2);
+  });
+
+  it("it should return the price 59 for the product F", function () {
+    expect(Supermarket.getPriceFor("F")).toEqual(59);
+  });
+
+  it("it should throw an error in the product doesn't exist", function () {
+    expect(function () {
+      Supermarket.getPriceFor("Z")
+    }).toThrow(); 
+  });
+
+  it("it should return the sum of the product prices", function () {
+    expect(Supermarket.getPriceFor("BB")).toEqual(26); 
+  });
 });
